4bc980ec9c5c78d3ded15783013bc4d9be7d2352
hu.jiabin88 <hu.jiabin88@gmail.com>
2011-12-03 04:03:30 +0000

    Modify test scripts to include interpreter tests

--- test/test.bash
+++ test/test.bash
@@ -5,7 +5,7 @@
 
 #!/bin/bash
 
-if [ $# -lt 1 ]
+if [ $# -lt 2 ]
 then
 	echo "No path for the compiler specified, use ../src/TML_Compiler/tmlc as default" 
 	tmlc="../src/TML_Compiler/tmlc"
@@ -19,12 +19,25 @@ then
 	exit
 fi
 
+echo "SrcCode tests"
 for file in SrcCode/*.tml
 do
-	echo "$tmlc < $file"
+	echo "Testing $file"
 	$tmlc < $file
 	filename=`expr $file : '\(^.*\)'[.]`
 	output=$filename.output
+	#echo "Compare with $output"
+done
+
+echo ""
+echo "ByteCode tests"
+for file in Bytecode/*.bc
+do
+	echo "Testing $file"
+	filename=`expr $file : '\(^.*\)'[.]`
+	java -classpath ../src/TML_Interpreter Main $file > $filename.result
+	stdput=$filename.output
+	diff $filename.result $stdput
 	#echo "Compare with $output"
 done	
 
