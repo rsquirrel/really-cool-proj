b8eacae6644289a4f4a21058d1d101b96d9016d8
hu.jiabin88 <hu.jiabin88@gmail.com>
2011-12-16 16:05:30 +0000

    Update the test script to compare results. Add option to run single tests.

--- test/test.bash
+++ test/test.bash
@@ -1,16 +1,24 @@
-# Author: Yan Zou
-# Usage: test.bash <path>
-#        The path is for the compiler.
-#        If no path is specified, ../src/TML_Compiler/tmlc is used as default
+# Author: Yan Zou, Jiabin Hu
+# Usage: test.bash <srcfile>
+#        srcfile: the source file to be tested.
+#        If no srcfile is specified, it will run all test cases.
 
 #!/bin/bash
 
-if [ $# -lt 2 ]
+tmlc="../src/TML_Compiler/tmlc"
+
+if [ $# -lt 1 ]
 then
-	echo "No path for the compiler specified, use ../src/TML_Compiler/tmlc as default" 
-	tmlc="../src/TML_Compiler/tmlc"
+	echo "Running all tests..." 
 else
-	tmlc=$1
+	file=$1
+    echo "Testing $file"
+    $tmlc $file
+    filename=`expr $file : '\(^.*\)'[.]`
+    java -classpath ../src/TML_Interpreter Main $filename.tmb > $filename.result
+    output=$filename.output
+    diff $filename.result $output
+	exit 0
 fi
 
 if [ ! -e "$tmlc" ]
@@ -25,20 +33,22 @@ do
 	echo "Testing $file"
 	$tmlc $file
 	filename=`expr $file : '\(^.*\)'[.]`
+	java -classpath ../src/TML_Interpreter Main $filename.tmb > $filename.result
 	output=$filename.output
+	diff $filename.result $output
 	#echo "Compare with $output"
 done
 
-echo ""
-echo "ByteCode tests"
-for file in Bytecode/*.bc
-do
-	echo "Testing $file"
-	filename=`expr $file : '\(^.*\)'[.]`
-	java -classpath ../src/TML_Interpreter Main $file > $filename.result
-	stdput=$filename.output
-	diff $filename.result $stdput
-	#echo "Compare with $output"
-done	
+#echo ""
+#echo "ByteCode tests"
+#for file in Bytecode/*.bc
+#do
+#	echo "Testing $file"
+#	filename=`expr $file : '\(^.*\)'[.]`
+#	java -classpath ../src/TML_Interpreter Main $file > $filename.result
+#	stdput=$filename.output
+#	diff $filename.result $stdput
+#	#echo "Compare with $output"
+#done	
 
 echo "Tests done."
